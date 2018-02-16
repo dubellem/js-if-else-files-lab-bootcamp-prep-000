5e8fbcb17b63476fcc9abb08d35573222990f4f5
Molyneau DuBelle <molyneau@gmail.com>
2018-02-16 13:53:37 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -1,10 +1,9 @@
 var index = require("./index.js")
-console.log(index.name)
+
 
 if (index.name === "Joe") {
     console.log("The name is correct")
 }
-
 
 if (index.height === 70) {
     console.log("The height is correct")
