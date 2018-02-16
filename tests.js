1f74cd8c6cbdbe9133cfdbd96951de82de222302
Molyneau DuBelle <molyneau@gmail.com>
2018-02-16 13:53:04 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -3,13 +3,9 @@ console.log(index.name)
 
 if (index.name === "Joe") {
     console.log("The name is correct")
-} else {
-    console.log("Expected: Susan, Received: "+index.name)
 }
 
 
 if (index.height === 70) {
     console.log("The height is correct")
-} else {
-    console.log("Expected: 70, Received: "+index.height)
 }
